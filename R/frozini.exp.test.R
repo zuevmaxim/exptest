e9bf9ec47de972f92a2820501b2a514d9ea75335
Ruslan Pusev <r.pusev@spbu.ru>
2013-11-30 00:00:00 +0000

    version 1.2

--- R/frozini.exp.test.R
+++ R/frozini.exp.test.R
@@ -8,11 +8,11 @@ y<-mean(x)
 b<-(1/sqrt(n))*sum(abs(1-exp(-x/y)-(c(1:n)-0.5)/n))
 for(i in 1:nrepl)
 {
-z<-rexp(n)
-z<-sort(z)
-y<-mean(z)
-B<-(1/sqrt(n))*sum(abs(1-exp(-z/y)-(c(1:n)-0.5)/n))
-if (B>b) l=l+1
+	z<-rexp(n)
+	z<-sort(z)
+	y<-mean(z)
+	B<-(1/sqrt(n))*sum(abs(1-exp(-z/y)-(c(1:n)-0.5)/n))
+	if (B>b) l=l+1
 }
 p.value<-l/nrepl
 RVAL<-list(statistic=c(B=b), p.value=p.value, method="Frozini test for exponentiality",data.name = DNAME)
